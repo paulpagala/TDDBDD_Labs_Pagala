3c2c6ae76043b852bf70c2c6f0c0a8161c4da6a7
EC2 Default User <ec2-user@ip-172-31-25-137.ap-southeast-1.compute.internal>
2022-07-21 02:50:11 +0000

    TDD lab 3

---
+++ test_lab3.py
@@ -0,0 +1,15 @@
+import pytest
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+calc = Calculator()
+
+#print(calc.add(1, 1))
+
+def test_add():
+    assert calc.add(1, 1) == 2
+    assert calc.add(1.0, 2.5) == 3.5
+    assert calc.add(0,  0) == 0 
+    assert calc.add(-5,-6) == -11
